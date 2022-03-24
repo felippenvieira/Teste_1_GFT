a198e5bdeacc547594d82cffc122663d96a10815
Felippe <felippe_nvieira@hotmail.com>
2022-03-24 16:28:42 -0300

    Add project files.

---
+++ Teste_1_GFT/Program.cs
@@ -0,0 +1,11 @@
+﻿var password = "2018";
+
+string userInput = Console.ReadLine();
+
+while (userInput != password)
+{
+    Console.WriteLine("Senha inválida");
+    userInput = Console.ReadLine();
+}
+
+Console.WriteLine("Acesso permitido");
